7f94955026f090762afb2ee50eaaaacf9614b7dc
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2020-06-11 12:02:00 +0300

    testng changes

--- src/test/java/cucumber/RunCucumberTest.java
+++ src/test/java/cucumber/RunCucumberTest.java
@@ -1,5 +1,5 @@
 package cucumber;
-
+/*
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
@@ -7,4 +7,20 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"})
 public class RunCucumberTest {
+}
+*/
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(plugin = { "html:target/results.html", "message:target/results.ndjson" })
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
 }
--- src/test/java/cucumber/StepDefinitions.java
+++ src/test/java/cucumber/StepDefinitions.java
@@ -7,12 +7,14 @@ import pageObjects.LoginPage;
 import pageObjects.SecureAreaPage;
 import pageObjects.WelcomePage;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 import java.net.MalformedURLException;
 
-import org.junit.Assert;
+//import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import general.TestBase;
 
@@ -39,13 +41,14 @@ public class StepDefinitions extends TestBase{
 	public void i_enter_tomsmith_in_username_field(String userName,String password) {
 		secureAreaPage=loginPage.enterUserNamePassword(userName, password);
 	}
+	@Test(description="Check that Secure Area page is opened")
 	@Then("Secure Area page is opened")
 	public void secure_Area_page_is_opened() {
 		String expectedSecureAreaPageTitle="Secure Area";
 		String actualSecureAreaPageTitle=secureAreaPage.getTitle();
 		Assert.assertEquals(actualSecureAreaPageTitle, expectedSecureAreaPageTitle);
 	}
-
+    @Test(description="Check that You logged into a secure area! message appears")
 	@Then("You logged into a secure area! message appears")
 	public void you_logged_into_a_secure_area_message_appears() {
 		String actualSecureAreaPageMsg=secureAreaPage.getMessage();
@@ -56,6 +59,7 @@ public class StepDefinitions extends TestBase{
 	public void incorrectUserName(String userName,String password) {
 		secureAreaPage=loginPage.enterUserNamePassword(userName, password);
 	}
+	@Test(description="Check that error message appears")
 	 @Then("{string} message appears")
 	 public void errorMessage_appears(String errorMessage) {
 			String actualSecureAreaPageMsg=loginPage.getMessage();
